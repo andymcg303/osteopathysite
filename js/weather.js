401c3795c52bc7c1de23d9e3ac4d491eaa1196a0
andymcg303 <mcgow_a@hotmail.com>
2019-07-13 18:55:25 +0100

    Added weather info to FAQs

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+var oReq = new XMLHttpRequest();
+
+oReq.addEventListener("load", function(){
+    if (oReq.status === 200){
+        var responseObject = JSON.parse(oReq.responseText);
+        // console.log(responseObject.weather[0].icon);	
+		var wDescription = (responseObject.weather[0].description);
+        // console.log(responseObject.main.temp);
+        var wTemp = parseInt(Number(responseObject.main.temp) - 273.15);
+        var wWindSpeed = (responseObject.wind.speed);
+        // console.log(responseObject.clouds.all);
+        var wString = "The weather is forecast to be " + wDescription + ", the temperature is " + wTemp + " degrees celsius and the windspeed is " + wWindSpeed + "kph";
+
+        var weather = document.querySelector("#weather");
+        weather.textContent = wString;
+    }
+});
+  
+oReq.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=Enfield,uk&APPID=a2f590296ef709ae67a363c31331ea1f");
+oReq.send();
